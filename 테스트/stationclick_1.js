5815fe63b10aa72d76a19c2afd8c997bdf9da030
M-C-L <102340042+M-C-L@users.noreply.github.com>
2023-06-15 21:49:36 +0900

    Delete 테스트 directory

--- 테스트/stationclick_1.js
+++
@@ -1,5 +0,0 @@
-var clicktest = document.getElementById("M1916");
-clicktest.onclick=function(){
-    var stationID=clicktest.getAttribute("stationID");
-    console.log("소요산 역의 역코드:",stationID);
-}
